a3be8e664947bf96eb1ea3a4f891b4f0e93fc007
Pedro Vitor Augusto de Jesus <pedrovitora.jesus@gmail.com>
2024-03-03 18:24:57 -0300

    feat: adiciona status code

--- src/models.py
+++ src/models.py
@@ -1,6 +1,29 @@
-from sqlalchemy import VARCHAR, Boolean, Column, ForeignKey, Integer
+from sqlalchemy import VARCHAR, Column, ForeignKey, Integer, Table
 from sqlalchemy.orm import relationship
 
 from database import Base
 
 
+# Tabela de associação entre Trainer e Pokemon
+trainer_pokemon_association = Table(
+    "trainer_pokemon_association",
+    Base.metadata,
+    Column("trainer_id", Integer, ForeignKey("TRAINER.id")),
+    Column("pokemon_id", Integer, ForeignKey("POKEMON.id"))
+)
+
+class Trainer(Base):
+    __tablename__ = "TRAINER"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    name = Column(VARCHAR(255), unique=True, index=True)
+    pokemons = relationship("Pokemon", secondary=trainer_pokemon_association, back_populates="trainers")
+
+class Pokemon(Base):
+    __tablename__ = "POKEMON"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(VARCHAR(255), unique=True, index=True)
+    weight = Column(Integer)
+    height = Column(Integer)
+    trainers = relationship("Trainer", secondary=trainer_pokemon_association, back_populates="pokemons")
